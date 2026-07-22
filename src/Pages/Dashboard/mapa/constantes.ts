97616a6e43f035e4fd5329f9955a33bc799f6a77
Yara Vazquez Molina <233379@ids.upchiapas.edu.mx>
2026-07-21 22:31:26 -0600

    Revert "Merge pssi: integrar mapa interactivo conservando datos reales de develop"
    This reverts commit 2312a07c95030d6540683ecabe1b25bb935d60e6, reversing
    changes made to 1a8d4c1ef911343eeb9ec5f7aff3b0475ac8532c.

--- src/Pages/Dashboard/mapa/constantes.ts
+++
@@ -1,8 +0,0 @@
-import type { Coordenada } from './geo';
-
-export const SUCHIAPA_CENTER: Coordenada = [16.6166, -93.1];
-
-export const SUCHIAPA_BOUNDS: [Coordenada, Coordenada] = [
-  [16.58, -93.15],
-  [16.66, -93.05],
-];
--- src/Pages/Dashboard/mapa/geo.ts
+++
@@ -1,1 +0,0 @@
-export type Coordenada = [number, number];
